e59336d3a9604105b8a3a806a118cd62ccfed26f
Emmaous <emmaogah97@gmail.com>
2020-07-26 23:56:15 +0100

    Created basic stucture for home page

---
+++ wordsearchapp/pages/index.tsx
@@ -0,0 +1,11 @@
+
+
+const HomePage = () => {
+  return <div>
+    <h1>Word Search</h1>
+    <div><button>Create Game</button></div>
+    <div><button>Join Game</button></div>
+  </div>
+}
+
+export default HomePage;
